836213ed3e1a866a147e3bffa07e779b44967270
Cosmin Stejerean <cosmin@offbytwo.com>
2020-04-25 16:21:55 -0700

    add personalizations

---
+++ personal/preload/solarized-dark.el
@@ -0,0 +1,5 @@
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/personal/themes/emacs-color-theme-solarized")
+(set-terminal-parameter nil 'background-mode 'dark)
+
+(setq prelude-theme 'solarized)
